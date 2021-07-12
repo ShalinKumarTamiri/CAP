82ecd1228d16956017f24d1656fa702c0c10775d
ShalinKumarTamiri <64578218+ShalinKumarTamiri@users.noreply.github.com>
2021-07-12 15:10:48 +0530

    Create theCoinChange.java

---
+++ DynamicProgramming/theCoinChange.java
@@ -0,0 +1,86 @@
+/*
+Given an amount and the denominations of coins available, determine how many ways change can be made for amount.
+There is a limitless supply of each coin type.
+
+Function Description
+
+Complete the getWays function in the editor below.
+
+getWays has the following parameter(s):
+
+int n: the amount to make change for
+int c[m]: the available coin denominations
+
+Returns
+
+int: the number of ways to make change
+
+*/
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getWays' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. LONG_INTEGER_ARRAY c
+     */
+
+    public static long getWays(int n, List<Long> c) {
+    // Write your code here
+        long[] count = new long[n+1];
+        count[0] = 1L; 
+        for(int i = 0; i < c.size(); i++){
+            for(long j = c.get(i); j < n+1; j++){
+                count[(int)j] += count[(int)(j-c.get(i))];
+            }
+        }
+        return count[n]; 
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Long> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+        long ways = Result.getWays(n, c);
+
+        bufferedWriter.write(String.valueOf(ways));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
